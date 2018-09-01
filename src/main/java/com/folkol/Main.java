f241d343b69d15222624c510fb335ba835f25593
Matte <mattias4@kth.se>
2018-09-01 12:30:18 +0200

    Basic reproducing test case.

--- src/main/java/com/folkol/Main.java
+++ src/main/java/com/folkol/Main.java
@@ -1,45 +1,77 @@
 package com.folkol;
 
+import java.net.URI;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
+import javax.ws.rs.GET;
+import javax.ws.rs.PUT;
+import javax.ws.rs.Path;
+import javax.ws.rs.QueryParam;
+import javax.ws.rs.client.Client;
+import javax.ws.rs.client.ClientBuilder;
+import javax.ws.rs.client.Entity;
+import javax.ws.rs.client.WebTarget;
+import javax.ws.rs.core.Response;
 import org.glassfish.grizzly.http.server.HttpServer;
-import org.glassfish.jersey.grizzly2.httpserver.GrizzlyHttpServerFactory;
 import org.glassfish.jersey.server.ResourceConfig;
 
-import java.io.IOException;
-import java.net.URI;
+import static org.glassfish.jersey.grizzly2.httpserver.GrizzlyHttpServerFactory.*;
 
 /**
- * Main class.
- *
+ * <ul>
+ * <li>http://jersey.576304.n2.nabble.com/Is-Jersey-Jersey-client-duplicating-requests-td6570645.html</li>
+ * <li>https://stackoverflow.com/questions/37956741/jersey-resource-receiving-duplicate-requests-from-jersey-client</li>
+ * </ul>
  */
 public class Main {
-    // Base URI the Grizzly HTTP server will listen on
-    public static final String BASE_URI = "http://localhost:8080/myapp/";
+    static final int N_THREADS = 100;
+    static final String BASE_URI = "http://localhost:8080/";
+    static final int N = 10_000;
 
-    /**
-     * Starts Grizzly HTTP server exposing JAX-RS resources defined in this application.
-     * @return Grizzly HTTP server.
-     */
-    public static HttpServer startServer() {
-        // create a resource config that scans for JAX-RS resources and providers
-        // in com.folkol package
-        final ResourceConfig rc = new ResourceConfig().packages("com.folkol");
+    @Path("")
+    public static class Resource {
+        static volatile int[] ids = new int[N];
 
-        // create and start a new instance of grizzly http server
-        // exposing the Jersey application at BASE_URI
-        return GrizzlyHttpServerFactory.createHttpServer(URI.create(BASE_URI), rc);
+        @PUT
+        public void put(@QueryParam("id") int id) {
+            if (ids[id]++ != 0) {
+                System.out.println("Duplicate request: " + id);
+            }
+        }
+
+        @GET
+        public int get(@QueryParam("id") int id) {
+            return ids[id];
+        }
     }
 
-    /**
-     * Main method.
-     * @param args
-     * @throws IOException
-     */
-    public static void main(String[] args) throws IOException {
-        final HttpServer server = startServer();
-        System.out.println(String.format("Jersey app started with WADL available at "
-                + "%sapplication.wadl\nHit enter to stop it...", BASE_URI));
-        System.in.read();
-        server.stop();
+    public static void main(String[] args) throws Exception {
+        ResourceConfig rc = new ResourceConfig().register(new Resource());
+//        rc.property("jersey.config.server.tracing.type", "ALL");
+//        rc.property("jersey.config.server.tracing.threshold", "VERBOSE");
+        HttpServer server = createHttpServer(URI.create(BASE_URI), rc);
+
+        Client client = ClientBuilder.newClient();
+        WebTarget target = client.target(BASE_URI);
+
+        ExecutorService es = Executors.newFixedThreadPool(N_THREADS);
+        for (int i = 0; i < N; i++) {
+            int id = i;
+            es.submit(() -> {
+                Response response = target.queryParam("id", id).request().put(Entity.json(""));
+                response.close();
+            });
+            es.submit(() -> {
+                Response response = target.queryParam("id", 0).request().get();
+//                response.close();
+//                response.readEntity(String.class);
+            });
+        }
+
+        es.shutdown();
+        es.awaitTermination(1, TimeUnit.MINUTES);
+        server.shutdown();
     }
 }
 
